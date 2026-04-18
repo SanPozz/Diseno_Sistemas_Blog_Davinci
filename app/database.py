ddf167808dfea760a92d8087957414e007351a19
Santiago Pozzolo <pozzolosanti@gmail.com>
2026-04-18 18:50:31 -0300

    Modelos importados en database.py

--- app/database.py
+++ app/database.py
@@ -9,6 +9,7 @@ def init_db(app):
 
         from app.models.Users import User;
         from app.models.Posts import Post;
+        from app.models.Categories import Category;
 
         db.create_all();
 
